403e65d17cc5d2da01d7203a6f8adb5156e58afb
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-08-04 18:41:21 +0530

    Commit

---
+++ EvenOddBitwise.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class EvenOddBitwise {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number:");
+            int num = sc.nextInt();
+            if ((num & 1) == 1) {
+                System.out.println("Given number is Even");
+            }
+            else{
+                System.out.println("Number is odd");
+            }
+        }
+    }
+}
